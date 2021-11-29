a87eaca7c54b3c654a12fa26447aafed9cf10f1d
Laurent Montel <montel@kde.org>
2021-11-29 06:56:46 +0100

    Fix license

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,2 +1,5 @@
 #! /bin/sh
+# SPDX-FileCopyrightText: 2014 Montel Laurent <montel@kde.org>
+# SPDX-License-Identifier: BSD-3-Clause
+
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/kontactinterfaces5.pot
