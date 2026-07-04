8139e9c2bbc85a42ae15d50b386b08a829233437
Allen Winter <winter@kde.org>
2026-07-04 11:03:26 -0400

    cppcheck cstyleCast fixes
    Fixes:
    ```
    + src/processes.cpp:
      -  139:C-style pointer casting
      -  140:C-style pointer casting
    ```

--- src/processes.cpp
+++ src/processes.cpp
@@ -136,8 +136,8 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
         DWORD pidwin;
 
         GetWindowThreadProcessId(hwnd, &pidwin);
-        if (pidwin == ((EnumWindowsStruct *)lParam)->pid) {
-            ((EnumWindowsStruct *)lParam)->windowId = hwnd;
+        if (pidwin == (struct<EnumWindowsStruct *>(lParam))->pid) {
+            (static_cast<EnumWindowsStruct *>(lParam))->windowId = hwnd;
             return FALSE;
         }
     }
